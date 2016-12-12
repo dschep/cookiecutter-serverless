389effe452aa40270c9680bd5831ff4de8092c68
Daniel Schep <dschep@gmail.com>
2016-12-11 23:11:07 -0500

    error handling

--- handler.js
+++ handler.js
@@ -5,5 +5,6 @@ module.exports.starLogger = (event, context, callback) => {
   fetch(`https://api.github.com/repos/${process.env.repo}`)
     .then((resp) => resp.json())
     .then((json) => fetch(`${process.env.phantUrl}/input/${process.env.phantPublicKey}?private_key=${process.env.phantPrivateKey}&stars=${json.stargazers_count}`))
-    .then((resp) => callback(null, {statusCode: 200, body: resp.text()}));
+    .then((resp) => callback(null, {statusCode: 200, body: resp.text()}))
+    .catch((err) => callback(err, null));
 };
